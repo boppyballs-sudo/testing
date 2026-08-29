8125e4f67de266b00c2b98393eeb1835168a90c8
boppyballs-sudo <boppyballs@gmail.com>
2026-08-29 18:43:47 -0400

    Update phantom forces.lua

--- phantom forces.lua
+++ phantom forces.lua
@@ -353,37 +353,6 @@ local function GetLockedPlayerName()
 	return "None"
 end
 
---========================================================--
---  CLICKDETECTOR FIXER
---========================================================--
-
-local function clickdt()
-	local Shop = workspace.Ignored.Shop
-	local cts = {"Guns", "Armor", "Others"}
-	for _, ctname in ipairs(cts) do
-		local cts = Shop:FindFirstChild(ctname)
-		if cts then
-			for _, item in ipairs(cts:GetChildren()) do
-				local clickDetector = item:FindFirstChildOfClass("ClickDetector")
-				if not clickDetector then
-					for _, child in ipairs(item:GetDescendants()) do
-						if child:IsA("ClickDetector") then
-							clickDetector = child
-							break
-						end
-					end
-				end
-				if clickDetector then
-					pcall(function()
-						clickDetector.MaxActivationDistance = math.huge
-					end)
-				end
-			end
-		end
-	end
-end
-
-clickdt() -- make it run whenever tf cus why not
 
 --========================================================--
 --  MOUSE METATABLE HOOK
